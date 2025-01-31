d95698f11e6781689688784c19e89976f09577a0
Reese Oo <reeseoo888@gmail.com>
2025-01-30 17:52:51 -0800

    Initial commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -34,4 +34,11 @@ public final class Constants {
     public static final double ARM_HOLD_DOWN = 0.1;
     public static final double ARM_HOLD_UP = -0.15;
   }
+
+  public static final class RollerConstants {
+      public static final int ROLLER_MOTOR_ID = 6;
+      public static final int ROLLER_MOTOR_CURRENT_LIMIT = 60;
+      public static final double ROLLER_MOTOR_VOLTAGE_COMP = 10;
+      public static final double ROLLER_EJECT_VALUE = 0.44;
+  }
 }
